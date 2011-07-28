283ff5e9a06c1c896351813840fc8ef4d8670481
Ricardo Villalobos Guevara <ricvgdev@gmail.com>
2011-07-27 20:52:12 -0400

    Completed example 3 of the turnstile.

--- src/fsm_turnstile_with_violation.py
+++ src/fsm_turnstile_with_violation.py
@@ -5,43 +5,57 @@ class SimpleTurnstile(object):
 
     class FSM(fsm.FSM):
         '''
-        Implementation of the FSM shown in [1]'s figure 2.
+        Implementation of the FSM shown in [1]'s figure 3.
 
         [1] UML Tutorial: Finite State Machines
             http://www.objectmentor.com/resources/articles/umlfsm.pdf
 
-                        --- Coin / unlock ---
-                       |                     |
-                       |                     V
-        Pass / Alarm  --------              ----------
-               ------|        |            |          |------
-              |      | Locked |            | Unlocked |      | Coin / Thankyou
-               ----->|        |            |          |<-----
-                      --------              ----------
-                         ^                     |
-                         |                     |
-                          ---- Pass / lock ----
+                     Pass        Pass                 Coin/
+                      ---     -- /alarm -----      -- unlock- -
+                     |   |   |               |    |            |
+        Reset/       |   V   V               |    |            V       Coin/
+        resetAlarm  -----------             --------     -----------   thankyou
+           ------->|           |           |        |   |           |------
+          |        | Violation |           | Locked |   | Unlocked  |      |
+           --------|           |           |        |   |           |<-----
+                    -----------             --------     -----------
+                     ^   |   |               ^    ^           |
+                     |   |   |               |    |           |
+                      ---     -- Ready/ -----      -- Pass/ --
+                     Coin    (resetAlarm,lock)        lock
         '''
         
         def __init__(self):
             locked = SimpleTurnstile.FSM.LockedState()
             unlocked = SimpleTurnstile.FSM.UnlockedState()
+            violation = SimpleTurnstile.FSM.ViolationState()
             
-            trans_to_unlocked = SimpleTurnstile.FSM.Transition(target=unlocked,
+            trans_to_unlocked = SimpleTurnstile.FSM.TransitionAndAction(target=unlocked,
                                             action=self.unlock)
-            trans_to_locked = SimpleTurnstile.FSM.Transition(target=locked,
+            trans_to_locked_and_lock = SimpleTurnstile.FSM.TransitionAndAction(target=locked,
                                             action=self.lock)
-            trans_to_unlocked_with_thankyou = SimpleTurnstile.FSM.Transition(
+            trans_to_unlocked_with_thankyou = SimpleTurnstile.FSM.TransitionAndAction(
                                             target=unlocked,
                                             action=self.thankyou)
-            trans_to_locked_with_alarm = SimpleTurnstile.FSM.Transition(
-                                                        target=locked,
+            trans_to_violation_with_alarm = SimpleTurnstile.FSM.TransitionAndAction(
+                                                        target=violation,
                                                         action=self.alarm)
             
             locked.add_transition(SimpleTurnstile.FSM.EventCoin, trans_to_unlocked)
-            locked.add_transition(SimpleTurnstile.FSM.EventPass, trans_to_locked_with_alarm)
-            unlocked.add_transition(SimpleTurnstile.FSM.EventPass, trans_to_locked)
+            locked.add_transition(SimpleTurnstile.FSM.EventPass, trans_to_violation_with_alarm)
+            unlocked.add_transition(SimpleTurnstile.FSM.EventPass, trans_to_locked_and_lock)
             unlocked.add_transition(SimpleTurnstile.FSM.EventCoin, trans_to_unlocked_with_thankyou)
+
+            trans_to_violation = SimpleTurnstile.FSM.Transition(target=violation)
+            trans_to_violation_and_resetAlarm = SimpleTurnstile.FSM.TransitionAndAction(
+                        target=violation, action = self.resetAlarm)
+            trans_to_locked_and_resetAlarmAndLock = SimpleTurnstile.FSM.TransitionAndAction(
+                        target=locked, action = self.resetAlarmAndLock)
+            
+            violation.add_transition(SimpleTurnstile.FSM.EventCoin, trans_to_violation)
+            violation.add_transition(SimpleTurnstile.FSM.EventPass, trans_to_violation)
+            violation.add_transition(SimpleTurnstile.FSM.EventReset, trans_to_violation_and_resetAlarm)
+            violation.add_transition(SimpleTurnstile.FSM.EventReady, trans_to_locked_and_resetAlarmAndLock)
 
             fsm.FSM.__init__(self, states = [locked, unlocked])
             self.set_initial_state(locked)
@@ -58,6 +72,13 @@ class SimpleTurnstile(object):
         
         def alarm(self, event):
             print "ALARM!"
+        
+        def resetAlarm(self, event):
+            print "Alarm reseted"
+        
+        def resetAlarmAndLock(self, event):
+            self.resetAlarm(event)
+            self.lock(event)
 
         class TurnstileEvent(fsm.Event):
             '''
@@ -78,9 +99,21 @@ class SimpleTurnstile(object):
             def __init__(self):
                 SimpleTurnstile.FSM.TurnstileEvent.__init__(self)
         
-        class Transition(fsm.TransitionWithAction):
+        class EventReset(TurnstileEvent):
+            def __init__(self):
+                SimpleTurnstile.FSM.TurnstileEvent.__init__(self)
+        
+        class EventReady(TurnstileEvent):
+            def __init__(self):
+                SimpleTurnstile.FSM.TurnstileEvent.__init__(self)
+        
+        class TransitionAndAction(fsm.TransitionWithAction):
             def __init__(self, target, action):
-                fsm.TransitionWithAction.__init__(self, target=target, effect=action)
+                fsm.TransitionWithAction.__init__(self, target=target, action=action)
+        
+        class Transition(fsm.Transition):
+            def __init__(self, target):
+                fsm.Transition.__init__(self, target=target)
         
         class TurnstileState(fsm.State):
             __metaclass__ = ABCMeta
@@ -99,6 +132,10 @@ class SimpleTurnstile(object):
             def __init__(self):
                 SimpleTurnstile.FSM.TurnstileState.__init__(self)
         
+        class ViolationState(TurnstileState):
+            def __init__(self):
+                SimpleTurnstile.FSM.TurnstileState.__init__(self)
+        
 
     def __init__(self):
         '''
@@ -114,6 +151,10 @@ class SimpleTurnstile(object):
                 self.dispatch_coin_event()
             elif key == 'p':
                 self.dispatch_pass_event()
+            elif key == 'r':
+                self.dispatch_reset_event()
+            elif key == 'a':
+                self.dispatch_ready_event()
             
     def __get_key(self):
         return raw_input('> ')
@@ -123,6 +164,12 @@ class SimpleTurnstile(object):
     
     def dispatch_pass_event(self):
         self.__dispatch_event(SimpleTurnstile.FSM.EventPass())
+    
+    def dispatch_reset_event(self):
+        self.__dispatch_event(SimpleTurnstile.FSM.EventReset())
+    
+    def dispatch_ready_event(self):
+        self.__dispatch_event(SimpleTurnstile.FSM.EventReady())
     
     def __dispatch_event(self, event):
         print 'Dispatching event: ', event
